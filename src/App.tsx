67e53de2c22bfd8cd256af65160f941db9763733
VanceGoblin <zvkubajak@gmail.com>
2025-02-03 21:25:57 -0600

    Styled Background

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 function App() {
   return (
-    <>
-      <div></div>
-    </>
+    <main className="relative">
+      <div className="absolute top-20 left-16 -z-10 bg-rose-200 h-80 w-72 rounded-full blur-3xl" />
+    </main>
   );
 }
 
