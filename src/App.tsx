8dbc5b6742adb28bd039346353610a5c978589e8
VanceGoblin <zvkubajak@gmail.com>
2025-02-03 22:11:04 -0600

    Created sections.ts to Store All Sections Data

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import Header from "./layout/Header";
+
 function App() {
   return (
-    <main className="relative">
-      <div className="absolute top-20 left-16 -z-10 bg-rose-200 h-80 w-72 rounded-full blur-3xl" />
-    </main>
+    <>
+      <Header />
+
+      <main className="relative">
+        <div className="absolute top-36 left-16 -z-10 bg-rose-200 h-72 w-72 rounded-full blur-3xl" />
+      </main>
+    </>
   );
 }
 
--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,8 +1,8 @@
 const Header = () => {
   return (
-    <div>
-      <div></div>
-    </div>
+    <header className="relative z-50">
+      <div className="fixed top-0 h-20 w-full bg-white opacity-75 border-b-2 border-b-gray-300 shadow-lg shadow-black/10"></div>
+    </header>
   );
 };
 
