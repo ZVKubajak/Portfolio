0bf30ebe42baa4ea0d673a2f3024b5c826f1ee9a
VanceGoblin <zvkubajak@gmail.com>
2025-02-04 11:06:54 -0600

    Created Intro Text & Picture w/ MQs

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,9 @@ function App() {
     <>
       <Header />
 
-      <main className="relative h-[5000px]">
-        <div className="absolute top-36 left-16 sm:left-44 md:left-60 lg:left-32 xl:left-72 2xl:left-96 -z-10 bg-rose-300 2xl:bg-rose-400 h-72 w-72 rounded-full blur-[120px] 2xl:blur-[150px]" />
-        <div className="hidden lg:block absolute top-52 right-32 xl:right-72 2xl:right-96 -z-10 bg-sky-300 2xl:bg-sky-400 h-72 w-72 rounded-full blur-[150px] 2xl:blur-[180px]" />
+      <main className="relative mt-32 h-[5000px]">
+        <div className="absolute top-24 left-16 sm:left-44 md:left-60 lg:left-32 xl:left-72 2xl:left-96 -z-10 bg-rose-300 2xl:bg-rose-400 h-72 w-72 rounded-full blur-[120px] 2xl:blur-[150px]" />
+        <div className="hidden lg:block absolute top-40 right-32 xl:right-72 2xl:right-96 -z-10 bg-sky-300 2xl:bg-sky-400 h-72 w-72 rounded-full blur-[150px] 2xl:blur-[180px]" />
 
         <Intro />
         <About />
--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,7 +1,43 @@
 const Intro = () => {
+  const date = new Date();
+  let greeting = "Hello";
+
+  if (date.getHours() < 12) {
+    greeting = "Good morning";
+  } else if (date.getHours() < 18) {
+    greeting = "Good afternoon";
+  } else if (date.getHours() < 24) {
+    greeting = "Good evening";
+  }
+
   return (
-    <section>
-      <div></div>
+    <section className="flex flex-col items-center space-y-8 mx-8 md:mx-28 lg:mx-56 xl:mx-80">
+      <img
+        src="https://ui-avatars.com/api/?name=John+Doe"
+        className="border-4 border-white shadow-lg rounded-full size-28"
+      />
+
+      <p className="sm:hidden text-center text-2xl">
+        Hey, I'm Zander! 👋 I am a{" "}
+        <span className="font-semibold">full-stack web developer</span>{" "}
+        specializing in{" "}
+        <span className="font-semibold">
+          TypeScript, React, TailwindCSS, Prisma,
+        </span>{" "}
+        and more.
+      </p>
+
+      <p className="hidden sm:block text-center text-2xl lg:text-3xl 2xl:text-4xl">
+        {greeting}! My name is Zander, and I am a{" "}
+        <span className="font-semibold">full-stack web developer</span>. I
+        specialize in{" "}
+        <span className="font-semibold">
+          TypeScript, React, TailwindCSS, Prisma,
+        </span>{" "}
+        and more. Creating{" "}
+        <span className="font-semibold">type-safe applications</span> is my
+        passion.
+      </p>
     </section>
   );
 };
