e07f80f2c58368c52754f28e1e80353e309ac768
VanceGoblin <zvkubajak@gmail.com>
2025-02-03 19:24:35 -0600

    Configured Vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,15 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/",
+  server: {
+    open: true,
+    port: 3000,
+    proxy: {
+      "/api": {
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
