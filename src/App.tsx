f7f02d3be7843927434d66e01d2494a1b5b6b828
VanceGoblin <zvkubajak@gmail.com>
2025-02-03 22:54:13 -0600

    Added MQs to Background & Added Map Logic to Section Links

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,8 @@ function App() {
       <Header />
 
       <main className="relative">
-        <div className="absolute top-36 left-16 -z-10 bg-rose-200 h-72 w-72 rounded-full blur-3xl" />
+        <div className="absolute top-36 left-16 sm:left-44 md:left-60 lg:left-32 xl:left-72 2xl:left-96 -z-10 bg-rose-300 2xl:bg-rose-400 h-72 w-72 rounded-full blur-[120px] 2xl:blur-[150px]" />
+        <div className="hidden lg:block absolute top-52 right-32 xl:right-72 2xl:right-96 -z-10 bg-sky-300 2xl:bg-sky-400 h-72 w-72 rounded-full blur-[150px] 2xl:blur-[180px]" />
       </main>
     </>
   );
--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,7 +1,15 @@
+import sections from "../lib/sections";
+
 const Header = () => {
   return (
     <header className="relative z-50">
-      <div className="fixed top-0 h-20 w-full bg-white opacity-75 border-b-2 border-b-gray-300 shadow-lg shadow-black/10"></div>
+      <div className="fixed top-0 w-full bg-white opacity-75 border-b-2 border-b-gray-300 shadow-lg shadow-black/10">
+        <div className="flex flex-wrap justify-evenly w-4/5 mx-auto text-xl space-x-4 py-2">
+          {sections.map((section, index) => (
+            <a key={index}>{section.name}</a>
+          ))}
+        </div>
+      </div>
     </header>
   );
 };
