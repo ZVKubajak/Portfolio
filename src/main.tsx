b72a58d28ffe8db656b39feb93c1d9ab8b878e9d
VanceGoblin <zvkubajak@gmail.com>
2025-02-03 21:28:38 -0600

    Created Header.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+
+import Header from "./layout/Header.tsx";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <Header />
     <App />
   </StrictMode>
 );
