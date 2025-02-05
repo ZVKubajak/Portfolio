b8423d60f74e6459fd00fca6dcf63a39eb11fffd
VanceGoblin <zvkubajak@gmail.com>
2025-02-05 08:34:06 -0600

    Created skills.ts

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,6 +1,11 @@
+import SectionHeading from "./SectionHeading";
+import skills from "../lib/skills";
+
 const Skills = () => {
   return (
-    <section>
+    <section className="mt-28 mx-12 sm:mx-20 space-y-8 text-center">
+      <SectionHeading>My Skills</SectionHeading>
+
       <div></div>
     </section>
   );
---
+++ src/lib/skills.ts
@@ -0,0 +1,21 @@
+const skills = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Node.js",
+  "Express",
+  "Prisma",
+  "PSQL",
+  "MongoDB",
+  "GraphQL",
+  "Bootstrap",
+  "Tailwind",
+  "Framer Motion",
+  "Python",
+  "Zod",
+  "AI",
+];
+
+export default skills;
