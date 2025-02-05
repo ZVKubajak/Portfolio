d4260ce7b86605ffe4056f1fce8fae9ae4bf371e
VanceGoblin <zvkubajak@gmail.com>
2025-02-05 11:28:23 -0600

    Added Motion Effects

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,19 +1,36 @@
 import SectionHeading from "./SectionHeading";
+import { motion } from "motion/react";
 import skills from "../lib/skills";
 
 const Skills = () => {
+  const fadeInAnimationVariants = {
+    initial: { y: 100, opacity: 0 },
+    animate: (index: number) => ({
+      y: 0,
+      opacity: 1,
+      transition: {
+        delay: 0.05 * index,
+      },
+    }),
+  };
+
   return (
     <section className="mt-28 mx-auto w-4/5 md:w-3/5 xl:w-1/2 text-center">
       <SectionHeading>My Skills</SectionHeading>
 
       <ul className="flex flex-wrap justify-center gap-2 md:gap-x-3 lg:gap-x-4 2xl:gap-y-3 mt-12 2xl:mt-16">
         {skills.map((skill, index) => (
-          <li
+          <motion.li
             key={index}
             className="border shadow-sm sm:shadow-md border-gray-500 rounded-lg bg-amber-100/15 text-gray-800 text-lg 2xl:text-xl px-3 sm:px-4 md:px-5 py-1 sm:py-2 lg:py-2.5 2xl:py-3"
+            variants={fadeInAnimationVariants}
+            initial="initial"
+            whileInView="animate"
+            viewport={{ once: true }}
+            custom={index}
           >
             {skill}
-          </li>
+          </motion.li>
         ))}
       </ul>
     </section>
