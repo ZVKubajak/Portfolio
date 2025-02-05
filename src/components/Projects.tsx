1ae13898a0eb6ca252ac941c848bd465e705416e
VanceGoblin <zvkubajak@gmail.com>
2025-02-05 14:30:57 -0600

    Created Start For projects.ts

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,6 +1,14 @@
+import SectionHeading from "./SectionHeading";
+
 const Projects = () => {
   return (
-    <section>
+    <section className="mt-28 mx-auto w-4/5 md:w-3/5 xl:w-1/2 text-center">
+      <SectionHeading>My Projects</SectionHeading>
+
+      <h2>Best Work</h2>
+
+      <h2>Other Projects</h2>
+
       <div></div>
     </section>
   );
---
+++ src/lib/projects.ts
@@ -0,0 +1,53 @@
+export const projects = [
+  {
+    name: "Stellabyte",
+    description: "",
+    site: "",
+    link: "https://github.com/ZVKubajak/Stellabyte",
+  },
+  {
+    name: "Digbi AI",
+    description: "",
+    site: "",
+    link: "https://github.com/ZVKubajak/Digbi-AI",
+  },
+  {
+    name: "codeBounty",
+    description: "",
+    site: "",
+    link: "https://github.com/ZVKubajak/codeBounty",
+  },
+  {
+    name: "Lifeguard Rotation Tracker",
+    description: "",
+    site: "",
+    link: "https://github.com/ZVKubajak/Lifeguard-Rotation-Tracker",
+  },
+] as const;
+
+export const otherProjects = [
+  {
+    name: "Quoteful",
+    description: "",
+    site: "",
+    link: "https://github.com/ZVKubajak/Quoteful",
+  },
+  {
+    name: "Book Page",
+    description: "",
+    site: "",
+    link: "https://github.com/ZVKubajak/book-page",
+  },
+  {
+    name: "Employee Tracker",
+    description: "",
+    site: "",
+    link: "https://github.com/ZVKubajak/employee-tracker",
+  },
+  {
+    name: "Cash Guardian",
+    description: "",
+    site: "",
+    link: "https://github.com/ZVKubajak/cash-guardian",
+  },
+] as const;
