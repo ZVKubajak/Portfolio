c89f9efce076ac77c41f0e601a8824042bce69f0
VanceGoblin <zvkubajak@gmail.com>
2025-02-08 09:35:14 -0600

    Added MQs

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -9,7 +9,7 @@ const Projects = () => {
     <section className="mt-28 mx-auto w-4/5 md:w-3/5 xl:w-1/2 text-center">
       <SectionHeading>My Projects</SectionHeading>
 
-      <h2 className="mt-12 text-2xl xl:text-3xl 2xl:text-4xl">Best Work</h2>
+      <h2 className="hidden xl:block mt-12 text-2xl xl:text-3xl 2xl:text-4xl">Best Work</h2>
 
       <div className="mt-8">
         {projects.map((project, index) => (
