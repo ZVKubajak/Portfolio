679b3d33fabeaab5bbb2a98f8d75ee5ec8e2a61a
VanceGoblin <zvkubajak@gmail.com>
2025-02-08 10:05:31 -0600

    Added Submit Button Icon

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,4 +1,5 @@
 import SectionHeading from "./SectionHeading";
+import { Send } from "lucide-react";
 
 const Contact = () => {
   return (
@@ -7,14 +8,20 @@ const Contact = () => {
 
       <p className="mt-4 md:mt-6 lg:mt-8 text-[15px] md:text-lg lg:text-xl">
         Contact me directly at{" "}
-        <span className="underline underline-offset-2">
+        <a
+          href="mailto:zvkubajak@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="underline underline-offset-2"
+        >
           zvkubajak@gmail.com
-        </span>{" "}
+        </a>{" "}
         or through this form.
       </p>
 
       <form className="flex flex-col mt-8 gap-y-4 md:text-lg lg:text-2xl sm:mx-auto sm:w-4/5 md:w-full lg:w-4/5 2xl:w-3/4">
         <input
+          type="email"
           placeholder="Email"
           className="bg-white border border-gray-200 rounded-lg px-2 py-1.5"
         />
@@ -24,8 +31,16 @@ const Contact = () => {
           className="resize-none h-32 lg:h-40 bg-white border border-gray-200 rounded-lg px-2 py-1.5"
         />
 
-        <button className="w-28 md:w-30 2xl:w-36 bg-slate-950 text-slate-50 text-xl md:text-2xl 2xl:text-3xl px-5 py-1 rounded-full">
+        <button
+          type="submit"
+          // className="flex w-28 md:w-30 2xl:w-36 bg-slate-950 text-slate-50 text-xl md:text-2xl 2xl:text-3xl px-5 py-1 rounded-full"
+          className="flex group bg-slate-950 text-slate-50 rounded-full text-xl md:text-2xl 2xl:text-3xl w-26 2xl:w-41 md:w-29.5 gap-x-2 px-5 py-1 cursor-pointer outline-none focus:scale-110 hover:scale-110 active:scale-105 transition"
+        >
           Submit
+          <Send
+            size={20}
+            className="hidden 2xl:block mt-2.5 group-hover:translate-x-0.5 group-hover:-translate-y-0.5 transition"
+          />
         </button>
       </form>
     </section>
