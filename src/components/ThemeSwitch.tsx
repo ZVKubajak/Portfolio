00455d2ed375ec284359737b51005bf07caa9100
VanceGoblin <zvkubajak@gmail.com>
2025-02-09 14:19:52 -0600

    Created ThemeSwitch.tsx

---
+++ src/components/ThemeSwitch.tsx
@@ -0,0 +1,11 @@
+import { Sun, Moon } from "lucide-react";
+
+const ThemeSwitch = () => {
+  return (
+    <button className="absolute">
+      <div></div>
+    </button>
+  );
+};
+
+export default ThemeSwitch;
