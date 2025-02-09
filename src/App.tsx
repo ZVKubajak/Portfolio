c698eb601df4f9a145ae77ba1018f8f071e12e9c
VanceGoblin <zvkubajak@gmail.com>
2025-02-09 14:09:31 -0600

    Added Footer Text & Styled w/ MQs

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
-import Footer from "./layout/Layout";
+import Footer from "./layout/Footer";
 
 function App() {
   return (
