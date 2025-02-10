4e247ac6ca7d0f2578286b3e37e7d9f4a51af3a7
VanceGoblin <zvkubajak@gmail.com>
2025-02-10 15:18:42 -0600

    Fixed Image Revert

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,8 +1,11 @@
+import footerLogoSVG from "../assets/svgs/footer-logo.svg";
+
 const Footer = () => {
   return (
     <footer className="text-center text-gray-600">
       <img
-        src="../../public/footer-logo.svg"
+        src={footerLogoSVG}
+        alt="Footer Logo"
         className="mx-auto mb-1 sm:mb-2 size-[45px] sm:size-[55px] md:size-[60px] lg:size-[65px] xl:size-[70px]"
       />
 
